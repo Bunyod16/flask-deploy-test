a58417d99d5f38acf442cba6d21d1a2067baa59b
Bunyod16 <bunyodshams@gmail.com>
2023-12-04 11:21:32 +0800

    init

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ app = Flask(__name__)
 visited = 0
 
 
-def visited_inc():
-    visited += 1
-    return visited
-
-
 @app.route("/")
 def hello_world():
-    return f"<p>Hello, World! Visited {visited_inc(visited)} times</p>"
+    return f"<p>Hello, World!</p>"
